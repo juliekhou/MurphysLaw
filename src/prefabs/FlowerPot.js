6876f326005cdcc90dcaeb069f51a658a246dfef
juliekhou <74749859+juliekhou@users.noreply.github.com>
2022-04-22 14:12:20 -0700

    Flower Class & background

--- src/prefabs/FlowerPot.js
+++ src/prefabs/FlowerPot.js
@@ -1,1 +1,23 @@
-// Flower Pot Class
+// Flower Pot Prefab
+class FlowerPot extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);   // add to existing scene
+        // this.points = pointValue;   // store pointValue
+        // this.moveSpeed = game.settings.spaceshipSpeed;         // pixels per frame
+        // this.moveSpeed = game.settings.spaceshipSpeed;         // pixels per frame
+    }
+
+    update() {
+        // move spaceship left
+        // this.x -= this.moveSpeed;
+        // wrap around from left edge to right edge
+        // if(this.x <= 0 - this.width) {
+        //     this.reset();
+        // }
+    }
+
+    reset(){
+        // this.x = game.config.width;
+    }
+}
